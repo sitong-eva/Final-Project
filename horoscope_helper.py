c2618ccb2077470c956b735af03c61d46f5cb992
sitong-eva <113207442+sitong-eva@users.noreply.github.com>
2023-04-29 21:09:20 -0400

    chaange the type of user input

--- horoscope_helper.py
+++ horoscope_helper.py
@@ -7,32 +7,32 @@ def get_sign(month, day):
     Given parameters month, day, returns astrological sign
     Source: https://www.w3resource.com/python-exercises/python-conditional-exercise-38.php
     """
-    month = str(month)
+    month = int(month)
     day = int(day)
     try:
-        if month == "12":
+        if month == 12:
             astro_sign = "Sagittarius" if (day < 22) else "capricorn"
-        elif month == "1":
+        elif month == 1:
             astro_sign = "Capricorn" if (day < 20) else "aquarius"
-        elif month == "2":
+        elif month == 2:
             astro_sign = "Aquarius" if (day < 19) else "pisces"
-        elif month == "3":
+        elif month == 3:
             astro_sign = "Pisces" if (day < 21) else "aries"
-        elif month == "4":
+        elif month == 4:
             astro_sign = "Aries" if (day < 20) else "taurus"
-        elif month == "5":
+        elif month == 5:
             astro_sign = "Taurus" if (day < 21) else "gemini"
-        elif month == "6":
+        elif month == 6:
             astro_sign = "Gemini" if (day < 21) else "cancer"
-        elif month == "7":
+        elif month == 7:
             astro_sign = "Cancer" if (day < 23) else "leo"
-        elif month == "8":
+        elif month == 8:
             astro_sign = "Leo" if (day < 23) else "virgo"
-        elif month == "9":
+        elif month == 9:
             astro_sign = "Virgo" if (day < 23) else "libra"
-        elif month == "10":
+        elif month == 10:
             astro_sign = "Libra" if (day < 23) else "scorpio"
-        elif month == "11":
+        elif month == 11:
             astro_sign = "scorpio" if (day < 22) else "sagittarius"
         return astro_sign.lower()
     except:
